ec17a16470776f83c462a180d04b89d8142872ba
Robiul Haque <robiulcoc420@gmail.com>
2024-06-28 00:41:43 +0600

    solve problem for update product qty & in stock when order a product

--- dist/modules/order/order.service.js
+++ dist/modules/order/order.service.js
@@ -13,10 +13,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.orderService = void 0;
+const product_model_1 = __importDefault(require("../product/product.model"));
 const order_model_1 = __importDefault(require("./order.model"));
 const createOrderIntoDB = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const res = yield order_model_1.default.create(payload);
-    return res;
+    var _a, _b;
+    const { productId, quantity } = payload;
+    const product = yield product_model_1.default.findById(productId);
+    if (!product) {
+        throw new Error('Product not found');
+    }
+    const totalProductQty = ((_a = product.inventory) === null || _a === void 0 ? void 0 : _a.quantity) - quantity;
+    if (totalProductQty !== 0 && ((_b = product.inventory) === null || _b === void 0 ? void 0 : _b.inStock) !== false) {
+        yield product_model_1.default.findByIdAndUpdate({ _id: productId }, { $set: { 'inventory.quantity': totalProductQty } });
+        const res = yield order_model_1.default.create(payload);
+        return res;
+    }
 });
 exports.orderService = {
     createOrderIntoDB,
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,10 +1,22 @@
-import { TProduct } from "../product/product.interface";
+import productModel from "../product/product.model";
+import { TOrder } from "./order.interface";
 import orderModel from "./order.model";
 
 
-const createOrderIntoDB = async (payload: TProduct) => {
-    const res = await orderModel.create(payload);
-    return res;
+const createOrderIntoDB = async (payload: TOrder) => {
+    const { productId, quantity } = payload;
+
+    const product = await productModel.findById(productId);
+    if (!product) {
+        throw new Error('Product not found');
+    }
+
+    const totalProductQty = product.inventory?.quantity - quantity;
+    if (totalProductQty !== 0 && product.inventory?.inStock !== false) {
+        await productModel.findByIdAndUpdate({ _id: productId }, { $set: { 'inventory.quantity': totalProductQty } });
+        const res = await orderModel.create(payload);
+        return res;
+    }
 }
 
 
